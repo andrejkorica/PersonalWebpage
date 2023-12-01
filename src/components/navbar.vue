3ff5d49a3d797bf3feedbe5a29f8312d92e0bc28
Andrej Korica <koricaandrej3@gmail.com>
2023-12-01 14:50:45 +0100

    Socials and a bit of responsive work

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,22 +1,28 @@
 <template>
-  <nav class="full-width z-50">
+  <nav class="full-width">
     <div
-      class="max-w-screen-xl flex flex-wrap items-center justify-between  p-8 ml-[5%]"
+      class="max-w-screen-xl flex flex-wrap items-center justify-between p-8 ml-0 md:ml-[5%]"
     >
       <div class="w-full flex">
         <ul class="flex justify-center">
           <li>
-            <a href="#" class="p-4 navItems">About me</a>
+            <a href="#" class="p-4 navItems text-base md:text-lg lg:text-xl"
+              >About</a
+            >
           </li>
           <li>
-            <a href="#" class="p-4 navItems">Projects</a>
+            <a href="#" class="p-4 navItems text-base md:text-lg lg:text-xl"
+              >Projects</a
+            >
           </li>
           <li>
-            <a href="mailto:koricaandrej3@gmail.com" class="p-4 navItems">Contact me</a>
+            <a
+              href="mailto:koricaandrej3@gmail.com"
+              class="p-4 navItems text-base md:text-lg lg:text-xl"
+              >Contact</a
+            >
           </li>
         </ul>
-
-
       </div>
     </div>
   </nav>
@@ -31,11 +37,17 @@ export default {};
   @apply relative ml-10 text-gray-500 font-mono font-bold tracking-[1px] text-xl;
 }
 
-.navItems::After {
-  @apply content-[''] bg-[#bc13fe] h-[3px] w-[100%] left-0 -bottom-[1px] rounded-xl absolute opacity-0 transition-opacity duration-300; 
+.navItems::after {
+  @apply content-[''] bg-[#bc13fe] h-[3px] w-[100%] left-0 -bottom-[1px] rounded-xl absolute opacity-0 transition-opacity duration-300;
 }
 
 .navItems:hover::after {
-  @apply opacity-100
+  @apply opacity-100;
+}
+
+.glowBurger{
+  -webkit-box-shadow:0px 0px 70px 19px rgba(187,19,254,0.39);
+  -moz-box-shadow: 0px 0px 70px 19px rgba(187,19,254,0.39);
+  box-shadow: 0px 0px 70px 19px rgba(187,19,254,0.39);
 }
 </style>
--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,93 +1,161 @@
 <template>
-      <div class="flex-1 flex items-center justify-center mt-[-100px]">
-      <div class="grid grid-cols-6 w-[80%]">
-        <!-- FIRST GRID ELEMENT -->
-        <div
-          class="col-span-3 flex flex-col items-start justify-center mx-auto"
+  <div class="flex-1 flex items-center justify-center md:mt-[-100px]">
+    <div class="grid grid-cols-6 w-[80%]">
+      <!-- FIRST GRID ELEMENT -->
+      <div
+        class="col-span-6 md:col-span-3 flex flex-col items-start justify-center mx-auto"
+      >
+        <h2
+          class="text-left mb-3 font-semibold text-white text-4xl lg:text-4xl sm:text-4xl md:text-4xl"
         >
-          <h2 class="text-left mb-5 text-5xl font-semibold text-white">
-            Hi, my name is Andrej Korica
-          </h2>
-          <h1 class="text-left mb-5 text-2xl font-semibold text-gray-400">
-            I am currently in my fifth year of college studying to become a
-            software developer
-          </h1>
-          <hr class="border-t-2 border-[#bc13fe] my-5 w-[100%] mx-0" />
+          Hi, my name is Andrej Korica
+        </h2>
 
-          <article class="flex whitespace-no-wrap overflow-x-hidden w-full">
-            <div class="relative">
-              <ul class="flex animate-marquee">
-                <li class="m-8">
-                  <img src="../assets/AndroidStudio.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/C++.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/CSharp.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/Java.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/JavaScript.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/Kotlin.png" class="w-[70px]" alt="">
-                </li>
-              </ul>
-              <ul class="flex absolute top-0 animate-marquee2">
-                <li class="m-8">
-                  <img src="../assets/MongoDB.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/Python.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/React.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/SQL.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/Unity.png" class="w-[70px]" alt="">
-                </li>
-                <li class="m-8">
-                  <img src="../assets/Vue.png" class="w-[70px]" alt="">
-                </li>
-              </ul>
-            </div>
-          </article>
+        <h1
+          class="text-left mb-5 text-xl md:text-2xl font-semibold text-gray-400"
+        >
+          I am currently in my fifth year of college studying to become a
+          software developer
+        </h1>
 
-          <button class="bg-transparen text-purple-500 font-semibold hover:text-purple-700 py-2 px-4 border border-purple-500 hover:border-purple-700  rounded mt-2">
-            Download my CV
-          </button>
-        </div>
+        <img
+          v-if="$windowWidth < 768"
+          src="../assets/korika.jpg"
+          class="w-full md:w-[400px] h-auto md:h-[400px]"
+          style="
+            box-shadow: 0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe,
+              0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
+          "
+        />
 
-        <!-- SECOND GRID ELEMENT -->
-        <div class="col-span-3 flex items-center justify-center">
-          <img
-            src="../assets/korika.jpg"
-            class="w-[400px] h-[400px]"
-            style="
-              box-shadow: 0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe,
-                0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
-            "
-          />
-        </div>
+        <hr class="border-t-2 border-[#bc13fe] my-5 w-[100%] mx-0" />
+
+        <article class="flex whitespace-no-wrap overflow-x-hidden w-full">
+          <div class="relative">
+            <ul class="flex animate-marquee">
+              <li class="md:m-8 m-3">
+                <img
+                  src="../assets/AndroidStudio.png"
+                  class="w-[70px]"
+                  alt=""
+                />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/C++.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/CSharp.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="sm:m-0 md:m-8 m-3">
+                <img src="../assets/Java.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/JavaScript.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/Kotlin.png" class="w-[70px]" alt="" />
+              </li>
+            </ul>
+            <ul class="flex absolute top-0 animate-marquee2">
+              <li class="md:m-8 m-3">
+                <img src="../assets/MongoDB.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/Python.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/React.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/SQL.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/Unity.png" class="w-[70px]" alt="" />
+              </li>
+              <li class="md:m-8 m-3">
+                <img src="../assets/Vue.png" class="w-[70px]" alt="" />
+              </li>
+            </ul>
+          </div>
+        </article>
+
+        <button
+          @click="downloadPDF()"
+          class="bg-transparen text-purple-500 font-semibold hover:text-purple-700 py-2 px-4 border border-purple-500 hover:border-purple-700 rounded mt-3 mx-auto md:mx-0"
+        >
+          Download my CV
+        </button>
+      </div>
+
+      <!-- SECOND GRID ELEMENT -->
+      <div
+        class="col-span-3 flex items-center justify-center"
+        v-if="$windowWidth > 768"
+      >
+        <img
+          src="../assets/korika.jpg"
+          class="w-[400px] h-[400px]"
+          style="
+            box-shadow: 0 0 0.2rem #fff, 0 0 0.2rem #fff, 0 0 2rem #bc13fe,
+              0 0 0.8rem #bc13fe, 0 0 2.8rem #bc13fe, inset 0 0 1.3rem #bc13fe;
+          "
+        />
       </div>
     </div>
+  </div>
+
+  <div class="pb-6 mt-6 cursor-pointer mx-auto">
+    <a
+      href="https://www.discordapp.com/users/korikaa"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i
+        class="fab fa-discord text-5xl text-white hover:text-[#5865F2] cursor-pointer"
+      ></i>
+    </a>
+    <a
+      href="https://www.linkedin.com/in/andrej-korica-9a83aa240/"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i
+        class="fa-brands fa-linkedin text-5xl text-white ml-5 hover:text-[#0077b5] cursor-pointer"
+      ></i>
+    </a>
+    <a
+      href="https://github.com/andrejkorica"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <i
+        class="fa-brands fa-github text-5xl text-white ml-5 hover:text-[#6e5494] cursor-pointer"
+      ></i>
+    </a>
+  </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    async downloadPDF() {
+      const cv_path = "http://localhost:5173/AndrejKorica.pdf";
+      const aTag = document.createElement("a");
+      aTag.href = cv_path;
 
-}
+      aTag.setAttribute("download", "AndrejKorica.pdf");
+      document.body.appendChild(aTag);
+      aTag.click();
+      aTag.remove();
+    },
+  },
+};
 </script>
 
 <style>
 @keyframes marquee {
-  0%, 100% {
+  0%,
+  100% {
     transform: translateX(0%);
   }
   50% {
@@ -96,7 +164,8 @@ export default {
 }
 
 @keyframes marquee2 {
-  0%, 100% {
+  0%,
+  100% {
     transform: translateX(100%);
   }
   50% {
@@ -110,5 +179,48 @@ export default {
 
 .animate-marquee2 {
   animation: marquee2 45s linear infinite;
+}
+.social-icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-bottom: 10px; /* Adjust as needed */
+}
+
+.social-icons a {
+  margin: 0 5px; /* Adjust the spacing between icons */
+}
+
+@media (max-width: 767px) {
+  .social-icons {
+    flex-direction: column; /* Stack icons vertically on small screens */
+    align-items: flex-end; /* Align icons to the right on small screens */
+  }
+}
+
+.social-icons {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin-bottom: 10px; /* Adjust as needed */
+}
+
+.social-icons a {
+  margin: 0 5px; /* Adjust the spacing between icons */
+}
+
+@media (max-width: 767px) {
+  .social-icons {
+    flex-direction: column; /* Stack icons vertically on small screens */
+    align-items: flex-end; /* Align icons to the right on small screens */
+  }
 }
 </style>
