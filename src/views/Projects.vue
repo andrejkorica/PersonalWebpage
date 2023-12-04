18445bd4b12b1399e34eaf29582c97acd81b46b3
Andrej Korica <koricaandrej3@gmail.com>
2023-12-04 19:09:33 +0100

    fixed readme pane, started working on api

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,33 +1,33 @@
 <template>
   <div class="w-[80%] mx-auto">
-    <div
-      class="grid | grid-cols-12 | h-[85vh] | p-4 | bg-white border | border-gray-200 | rounded-lg shadow | dark:bg-gray-800 | dark:border-gray-700 | overflow-auto"
-    >
-      <div class="col-span-3">
+    <!-- MAIN PARENT -->
+    <div class="flex h-[85vh] p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-auto">
+      
+      <!-- LEFT SIDE - Repository List -->
+      <div class="w-[25%] overflow-auto">
         <div v-for="data in githubAccount" :key="data.id">
           <div
-            class="p-3 | bg-white | border | border-gray-200 | rounded-lg | shadow | dark:bg-gray-900 | dark:border-gray-700"
+            @click="getMarkdown(data.owner.login, data.clone_url)" 
+            :title="data.name"
+            class="overflow-ellipsis hover:bg-gray-800 cursor-pointer p-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700"
           >
-            <h1
-              class="mb-1 | text-xl | font-medium | text-gray-900 | dark:text-white"
-            >
+            <h1 class="mb-1 text-xl font-medium text-gray-900 dark:text-white overflow-ellipsis line-clamp-2">
               {{ data.name }}
             </h1>
-            <p
-              v-if="data.description"
-              class="text-sm | text-gray-500 | dark:text-gray-400"
-            >
+            <p v-if="data.description" class="text-sm text-gray-500 dark:text-gray-400">
               {{ data.description }}
             </p>
-            <p v-else class="text-sm | text-gray-500 | dark:text-gray-400">
+            <p v-else class="text-sm text-gray-500 dark:text-gray-400">
               No description defined
             </p>
           </div>
         </div>
       </div>
-      <div
-        class=" col-span-9 | p-4 | bg-white border | border-gray-200 | rounded-lg shadow | dark:bg-white | dark:border-gray-700 | overflow-auto | ml-2"
-      ></div>
+
+      <!-- RIGHT SIDE - MD READER -->
+      <div class="flex-1 p-4 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-700 overflow-auto ml-2">
+        
+      </div>
     </div>
   </div>
 </template>
@@ -50,6 +50,37 @@ export default {
       } catch (error) {
         console.log(error);
       }
+    },
+    async getMarkdown(owner, clone_url) {
+      
+      
+      // Replace these values with your GitHub username, repository name, and file path
+      const ownerOfRepo = owner;
+      const repo = clone_url;
+      const path = "readme.md";
+
+      // Replace 'YOUR_ACCESS_TOKEN' with your GitHub personal access token if the repository is private
+      const accessToken = "YOUR_ACCESS_TOKEN";
+
+      // GitHub API endpoint URL
+      const apiUrl = `https://api.github.com/repos/${owner}/${repo}/contents/${path}`;
+
+      // Axios request to fetch the Markdown file
+      axios
+        .get(apiUrl, {
+          headers: {
+            Accept: "application/vnd.github.v3.raw",
+            Authorization: `Bearer ${accessToken}`, // Include this line if the repository is private
+          },
+        })
+        .then((response) => {
+          console.log(response.data);
+          // Now 'response.data' contains the content of the Markdown file
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+        
     },
   },
 
